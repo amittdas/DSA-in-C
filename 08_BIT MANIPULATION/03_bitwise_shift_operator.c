3f25641778309119b05452efd51efa8bbd3fe574
amittdas <amitdas123456789@gmail.com>
2026-03-26 00:44:52 +0530

    Added SHIFT OPERATOR

---
+++ 08_BIT MANIPULATION/03_bitwise_shift_operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // Left Shift operator
+    // (a << b) = a * (2^b)
+    printf("%d\n", (7 << 2));  // 28
+
+    // Right Shift operator
+    // (a >> b) = a / (2^b)
+    printf("%d\n", (7 >> 2));  // 1
+
+    return 0;
+}
