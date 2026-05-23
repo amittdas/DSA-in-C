f6f91ab7e1aa36d9cfb3ffdfec2e4c6d81e75d7d
amittdas <amitdas123456789@gmail.com>
2026-05-23 23:45:18 +0530

    Added check sorted or not

---
+++ 09_RECURSION/06_check_sorted.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isSorted(int arr[], int n, int i) {
+    if(i == (n - 1)) {
+        return true;
+    }
+
+    if(arr[i] > arr[i + 1]) {
+        return false;
+    }
+
+    return isSorted(arr, n, i + 1);
+}
+
+int main() {
+    int arr1[5] = {1, 2, 3, 4, 5}; // sorted --> 1
+    int arr2[5] = {1, 2, 4, 3, 5}; // unsorted --> 0
+
+    printf("%d\n", isSorted(arr1, 5, 0));
+    printf("%d\n", isSorted(arr2, 5, 0));
+
+    return 0;
+}
